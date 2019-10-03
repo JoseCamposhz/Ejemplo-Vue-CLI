74597a88dc2ed230018e6695bc1b0a8844dc58b2
JoseCamposhz <48602563+JoseCamposhz@users.noreply.github.com>
2019-10-03 16:19:59 -0300

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+    routes: [
+      {
+        path: '/',
+        component: () => import('../App'),
+        children: [
+          {
+            path: '/',
+            name: 'Inicio',
+            component: () => import('../views/Inicio')
+          }
+          ,
+          {
+            path: '/form',
+            name: 'Formulario',
+            component: () => import('../views/Form')
+          }
+          ,
+          {
+            path: '/paises',
+            name: 'Paises',
+            component: () => import('../views/Paises')
+          }
+        ]
+      },
+      {
+        path:'*',
+        redirect: '/'
+      }
+    ]
+})
+
+export default router
---
+++ src/views/Inicio.vue
@@ -0,0 +1,73 @@
+<template>
+  <section>
+    <Navbar />
+    <v-layout wrap mt-6 justify-center>
+      <v-flex sm4>
+        <v-card class="mx-auto">
+          <v-img
+            class="white--text"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title class="align-end fill-height">I'm a title</v-card-title>
+          </v-img>
+          <v-card-text>I'm card text</v-card-text>
+          <v-card-actions>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+            <v-btn text>Click</v-btn>
+            <v-btn outlined>Click</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex sm12>
+      <v-sheet height="500">
+        <v-calendar
+          type="month"
+          now="2019-01-08"
+          value="2019-01-08"
+          :events="events"
+        ></v-calendar>
+      </v-sheet>
+      </v-flex>
+    </v-layout>
+  </section>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue"
+export default {
+  components: { Navbar },
+  data: () => ({
+      events: [
+        {
+          name: 'Vacation',
+          start: '2018-12-30',
+          end: '2019-01-02',
+        },
+        {
+          name: 'Meeting',
+          start: '2019-01-07',
+        },
+        {
+          name: '30th Birthday',
+          start: '2019-01-03',
+        },
+        {
+          name: 'New Year',
+          start: '2019-01-01',
+        },
+        {
+          name: 'Conference',
+          start: '2019-01-21',
+        },
+        {
+          name: 'Hackathon',
+          start: '2019-01-30',
+          end: '2019-02-01',
+        },
+      ]
+    })
+}
+</script>
